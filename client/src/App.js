73eec77841bdfdb4cb44885bf67cf8f05d0172cb
Diana <dianavw8@gmail.com>
2023-04-01 12:00:38 -0400

    cleaned up code in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Axios from 'axios';
-import { BrowserRouter as Switch, Route, HashRouter } from "react-router-dom";
+import { Route, HashRouter, BrowserRouter } from "react-router-dom";
 import SSHeader from "./components/Header";
 import Profile from "./pages/Profile";
 import Username from "./pages/Username";
@@ -39,19 +39,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <HashRouter >
         <>
           <SSHeader />
-          <Switch>
+          <BrowserRouter>
             <Route exact path="/" component={Main} />
             <Route exact path="/points" component={Points} />
             <Route exact path="/username" component={Username} />
             <Route exact path="/login" component={LoginForm} />
             <Route exact path="/signup" component={SignupForm} />
             <Route exact path="/profile" component={Profile} />
-          </Switch>
+          </BrowserRouter>
         </>
-      </HashRouter >
     </ApolloProvider>
   );
 }
